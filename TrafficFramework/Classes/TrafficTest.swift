fb8f3e65b7b6bf3c967c1c6b73e9333d4e977e8e
faraz <faraz@Faraz-Siddiqui.local>
2017-12-20 15:32:26 +0500

    Files updated

---
+++ TrafficFramework/Classes/TrafficTest.swift
@@ -0,0 +1,15 @@
+//
+//  TrafficTest.swift
+//  Pods-TrafficFramework_Example
+//
+//  Created by Faraz Hussain Siddiqui on 12/20/17.
+//
+
+import Foundation
+
+public class TrafficTest {
+    
+    public class func TrafficTest() {
+        print("Test successfull");
+    }
+}
